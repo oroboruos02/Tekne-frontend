b6daaa669f9187b6cd8eb8224f71e1ee8abba2d3
Divar Parra <arc.eseisnos@gmail.com>
2024-05-31 18:38:25 -0500

    posiciono boton subir

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -41,7 +41,7 @@ const ScrollToTopButton = () => {
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
     >
-      <div className="fixed bottom-2 right-6 z-50">
+      <div className="fixed bottom-2 right-4 z-50">
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow transition-colors duration-300"
           onClick={scrollToTop}
