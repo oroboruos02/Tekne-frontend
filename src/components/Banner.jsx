4653f91c7567f5cbb79a331ec6f11f85198fefd5
oroboruos02 <arc.eseisnos@gmail.com>
2024-09-30 10:41:09 -0500

    ajuste banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,34 +1,36 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-    return(
-        <div className="relative bg-gray-900">
-        {/* Decorative image and overlay */}
-        <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
-          <img
-            src="./banner.jpg"
-            alt=""
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
-        <div aria-hidden="true" className="absolute inset-0 bg-gray-900 opacity-50" />
-        <div className="relative mx-auto flex max-w-3xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0">
-          <h1 className="text-4xl font-bold tracking-tight text-white lg:text-6xl">¡Bienvenidos a Tekne! Excelencia en ingeniería, montajes eléctricos y automatización. Innovamos para su éxito.
-</h1>
-          <p className="mt-4 text-xl text-white">
-          Hacemos de tu proyecto nuestro compromiso.
-          </p>
-          <Link
-            to="/contact"
-            className="mt-8 inline-block rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out transform hover:scale-105"
-            >
-            Contáctenos
-          </Link>
-        </div>
+  return (
+    <div className="relative bg-gray-900">
+      {/* Imagen de fondo decorativa */}
+      <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
+        <img
+          src="./banner.jpg"
+          alt="Banner de Tekne Soluciones"
+          className="h-full w-full object-cover object-center"
+        />
       </div>
-        
-    )
-}
+      <div aria-hidden="true" className="absolute inset-0 bg-gray-900 opacity-50" />
 
-export default Banner
+      {/* Contenido del banner */}
+      <div className="relative mx-auto flex max-w-3xl flex-col items-center px-6 py-24 sm:py-48 lg:py-56 text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl lg:text-5xl">
+          ¡Bienvenidos a Tekne! Excelencia en ingeniería, montajes eléctricos y automatización.
+        </h1>
+        <p className="mt-4 text-lg sm:text-xl text-white">
+          Hacemos de tu proyecto nuestro compromiso.
+        </p>
+        <Link
+          to="/contact"
+          className="mt-8 inline-block rounded-md border border-transparent bg-white px-6 py-2 sm:px-8 sm:py-3 text-base font-medium text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          Contáctenos
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
